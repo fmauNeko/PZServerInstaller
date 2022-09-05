22c48ce8dea1874900ecd42306370891c4d309ab
Florian Maunier <fmaunier@gmail.com>
2022-09-05 17:18:19 +0200

    feat(app): implement server config creation

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
+import glob from 'glob';
 import got from 'got'; // eslint-disable-line import/no-unresolved
+import ini from 'ini';
 import { SteamCmd } from 'steamcmd-interface';
 import toml from 'toml';
-import process from 'node:process';
-import { readFile } from 'node:fs/promises';
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
+import os from 'node:os';
 import path from 'node:path';
+import process from 'node:process';
 
-const rawConfig = await readFile(path.join(process.cwd(), 'config.toml'), 'utf-8');
+const rawConfig = await fsPromises.readFile(path.join(process.cwd(), 'config.toml'), 'utf-8');
 const config = toml.parse(rawConfig);
 
 const steamCmd = await SteamCmd.init({
@@ -52,3 +56,62 @@ const commands = allWorkshopIds.map((i) => `workshop_download_item 108600 ${i} v
 for await (const line of steamCmd.run(commands)) {
   console.log(line);
 }
+
+const serverDataPath = path.join(os.homedir(), 'Zomboid');
+
+try {
+  fsPromises.stat(serverDataPath);
+} catch {
+  console.error(`
+    ${serverDataPath} doesn't exist.
+    Please start the server for the first time using ${path.join(config.serverPath, 'start-server.sh')} then quit it and rerun this script.
+  `);
+  process.exit(1);
+}
+
+const modsPath = path.join(serverDataPath, 'Mods');
+
+try {
+  fsPromises.lstat(modsPath);
+} catch {
+  console.log(`
+    ${path.join(serverDataPath, 'Mods')} doesn't exist.
+    Creating a link from the workshop folder...
+  `);
+
+  const workshopModsPath = path.join(os.homedir(), 'Steam', 'steanapps', 'workshop', 'content', '108600');
+  try {
+    await fsPromises.link(workshopModsPath, modsPath);
+  } catch (err) {
+    console.error(`
+      Unable to create link ${workshopModsPath} -> ${modsPath}.
+      Reported error: ${err}.
+    `);
+    process.exit(1);
+  }
+}
+
+const modInfoPaths = glob.sync(path.join(modsPath, '**', 'mod.info'));
+
+const modLoadIds = modInfoPaths.map((p) => {
+  const modInfo = ini.parse(fs.readFileSync(p));
+  return modInfo.id;
+});
+
+const serverConfigPath = path.join(serverDataPath, 'Server', 'servertest.ini');
+
+let serverConfig = '';
+
+try {
+  serverConfig = await fsPromises.readFile(serverConfigPath);
+} catch {
+  console.error(`Cannot read ${serverConfigPath}`);
+  process.exit(1);
+}
+
+const parsedServerConfig = ini.parse(serverConfig);
+
+parsedServerConfig.WorkshopItems = allWorkshopIds.join(';');
+parsedServerConfig.Mods = modLoadIds.join(';');
+
+await fsPromises.writeFile(serverConfigPath, ini.stringify(parsedServerConfig));
